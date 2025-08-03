b2860d188727b0aec4254eefc990bf4244d3c0fe
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2025-08-03 16:26:13 +0200

    roslyn inlay hints and codelens support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,15 +9,44 @@
 
 vim.api.nvim_create_user_command("Wa", "wa", { desc = "wall" })
 
-vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
-vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-  callback = function() vim.highlight.on_yank({ timeout = 40 }) end,
-})
+-- vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--   pattern = "*",
+--   group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+--   callback = function() vim.highlight.on_yank({ timeout = 40 }) end,
+-- })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
   group = vim.api.nvim_create_augroup("copilot_insert_hide", { clear = true }),
   callback = function() vim.b.copilot_suggestion_hidden = true end,
   desc = "Hide copilot suggestion on insert enter",
+})
+
+vim.lsp.config("roslyn", {
+  on_attach = function()
+    vim.lsp.inlay_hint.enable(false)
+  end,
+  settings = {
+    ["csharp|inlay_hints"] = {
+      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+      csharp_enable_inlay_hints_for_types = true,
+      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+      dotnet_enable_inlay_hints_for_literal_parameters = true,
+      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+      dotnet_enable_inlay_hints_for_other_parameters = true,
+      dotnet_enable_inlay_hints_for_parameters = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+    },
+    ["csharp|code_lens"] = {
+      dotnet_enable_references_code_lens = true,
+    },
+    ["csharp|completion"] = {
+      dotnet_show_completion_items_from_unimported_namespaces = true,
+      dotnet_show_name_completion_suggestions = true,
+    },
+  },
 })
