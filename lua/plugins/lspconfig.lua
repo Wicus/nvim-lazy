a8ad291b789424c75fa8924803b6469160a74b9f
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2025-10-28 13:43:10 +0200

    Update to use latest lspconfig keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,28 +1,30 @@
+-- "grn" is mapped in Normal mode to |vim.lsp.buf.rename()|
+-- "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
+-- "grr" is mapped in Normal mode to |vim.lsp.buf.references()|
+-- "gri" is mapped in Normal mode to |vim.lsp.buf.implementation()|
+-- "gO" is mapped in Normal mode to |vim.lsp.buf.document_symbol()|
+-- CTRL-S is mapped in Insert mode to |vim.lsp.buf.signature_help()|
+
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    opts.inlay_hints = opts.inlay_hints or {}
-    opts.inlay_hints.enabled = false
+  opts = {
+    inlay_hints = { enabled = false },
+    servers = {
+      ["*"] = {
+        keys = {
+          { "gr", false },
+          { "grr", function() Snacks.picker.lsp_references() end, desc = "LSP References" },
+          { "gri", function() Snacks.picker.lsp_implementations() end, desc = "LSP Implementations" },
+          { "grt", function() Snacks.picker.lsp_type_definitions() end, desc = "LSP Type Definitions" },
+          { "gd", function() Snacks.picker.lsp_definitions() end, desc = "LSP Definitions" },
+          { "gh", vim.diagnostic.open_float, desc = "Show diagnostics" },
+          { "<C-s>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature help" },
 
-    -- "grn" is mapped in Normal mode to |vim.lsp.buf.rename()|
-    -- "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
-    -- "grr" is mapped in Normal mode to |vim.lsp.buf.references()|
-    -- "gri" is mapped in Normal mode to |vim.lsp.buf.implementation()|
-    -- "gO" is mapped in Normal mode to |vim.lsp.buf.document_symbol()|
-    -- CTRL-S is mapped in Insert mode to |vim.lsp.buf.signature_help()|
-
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    keys[#keys + 1] = { "gr", false }
-    keys[#keys + 1] = { "grr", function() Snacks.picker.lsp_references() end, desc = "LSP References" }
-    keys[#keys + 1] = { "gri", function() Snacks.picker.lsp_implementations() end, desc = "LSP Implementations" }
-    keys[#keys + 1] = { "grt", function() Snacks.picker.lsp_type_definitions() end, desc = "LSP Type Definitions" }
-    keys[#keys + 1] = { "gd", function() Snacks.picker.lsp_definitions() end, desc = "LSP Definitions" }
-    keys[#keys + 1] = { "gh", vim.diagnostic.open_float, desc = "Show diagnostics" }
-    keys[#keys + 1] = { "<C-s>", vim.lsp.buf.signature_help, desc = "Signature help" }
-
-    -- Code Lens
-    keys[#keys + 1] = { "<leader>cc", function() vim.lsp.codelens.refresh() end, desc = "Refresh CodeLens" }
-    keys[#keys + 1] = { "<leader>cC", function() vim.lsp.codelens.clear() end, desc = "Clear CodeLens" }
-  end,
+          -- Code Lens
+          { "<leader>cc", function() vim.lsp.codelens.refresh() end, desc = "Refresh CodeLens" },
+          { "<leader>cC", function() vim.lsp.codelens.clear() end, desc = "Clear CodeLens" },
+        },
+      },
+    },
+  },
 }
