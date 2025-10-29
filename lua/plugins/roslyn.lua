7c876ba8a09975defdf77b4ca7f287aa26a72649
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2025-10-29 21:49:06 +0200

    chore(roslyn): remove choost_target, it's not working

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,16 +1,7 @@
 return {
   "seblj/roslyn.nvim",
   ft = "cs",
-  opts = {
-    filewatching = "roslyn",
-    choose_target = function(target)
-      return vim.iter(target):find(function(item)
-        if string.match(item, "IoTnxt.Eskom.V5.sln") then
-          return item
-        end
-      end)
-    end,
-  },
+  opts = { filewatching = "roslyn" },
   keys = {
     { "<leader>lt", "<cmd>Roslyn target<cr>", desc = "LSP Roslyn (Select Target)" },
     { "<leader>ls", "<cmd>Roslyn stop<cr>", desc = "LSP Roslyn (Stop)" },
