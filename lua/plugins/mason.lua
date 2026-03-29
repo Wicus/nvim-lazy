c03575c5c5e42f5b2da4850ac1cae032b17ee5d3
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2026-03-29 18:54:49 +0200

    pinning roslyn lsp to 5.4.x and packages update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,6 +5,19 @@ return {
       "github:mason-org/mason-registry",
       "github:crashdummyy/mason-registry",
     },
-    ensure_installed = { "stylua", "csharpier", "prettierd", "black", "netcoredbg", "roslyn", "eslint_d" },
+    ensure_installed = {
+      "stylua",
+      "csharpier",
+      "prettierd",
+      "black",
+      "netcoredbg",
+      -- This is the last working version before 5.6: 5.4.0-2.26175.10 and the mason registry does not have it in it's registry. 
+      -- I have looked under the crashdummyy/mason-registry for tags, but the latest 5.4.x that they have is not available to dowload as 
+      -- a release anymore.
+      --
+      -- Disabling roslyn until there is a stable version. Run `:MasonInstall roslyn@5.4.0-2.26175.10` to install the last working version.
+      -- "roslyn", 
+      "eslint_d",
+    },
   },
 }
